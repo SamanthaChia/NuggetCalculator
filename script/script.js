49f31f283f646313ff3cad8b348fdcafe6de2ed6
Owen Lee <owenleeweihern@gmail.com>
2018-09-24 17:03:47 +0800

    Functions completed, css fixed.

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ function storeData(id) {
     getBills();
 }
 
-function getBills(){
+function getBills() {
     let bills = totalBills.reduce(reduceBillsArr);
     document.getElementById("total-bills").innerHTML = bills;
 }
@@ -17,15 +17,35 @@ function getBills(){
 function storeSalary(id) {
     let attrName = document.getElementById(id).id;
     let attrVal = document.getElementById(id).value;
+    let errorMsg = document.getElementById("error-msg");
+
     sessionStorage.setItem(attrName, attrVal);
+    errorMsg.innerHTML = "";
     getSalaryAmt();
+}
+
+function validateForm() {
+    var fields = document.getElementsByClassName("input-field").value;
+    console.log(fields);
+
+    if(fields == undefined)
+    {
+      document.getElementById("slide").style.backgroundColor = "red";
+      document.getElementById("error-msg").innerHTML= "* Field is required! ";
+      document.getElementById("error-msg").style.color= "red";
+      document.getElementById("error-msg").style.marginLeft = "80px";
+      document.getElementById("error-msg").style.fontStyle= "italic";
+    }
+
+
 }
 
 function getSalaryAmt() {
     let salaryAmt = sessionStorage.getItem("salary-amt");
     let slider = document.getElementById("save-amt");
+
     slider.setAttribute("max", salaryAmt);
-    document.getElementById("slider-field").appendChild(slider);
+    document.getElementById("slider").appendChild(slider);
     return salaryAmt;
 }
 
@@ -39,7 +59,7 @@ function createSlider() {
     slider.setAttribute("id", "save-amt");
     slider.setAttribute("min", "0");
     slider.setAttribute("max", maxVal);
-    document.getElementById("slider-field").appendChild(slider);
+    document.getElementById("slider").appendChild(slider);
 }
 
 function snapSavingsValue() {
@@ -47,4 +67,18 @@ function snapSavingsValue() {
     let slider = document.getElementById("save-amt");
 
     slider.value = snapVal;
+}
+
+function tabulateWeeklyBudget() {
+    let salary = sessionStorage.getItem("salary-amt");
+    parseInt(salary);
+    let totalBills = totalBills.reduce(reduceBillsArr);
+    parseInt(totalBills);
+    let amtAfterReduc = salary - totalBills;
+
+    let savings = document.getElementById("save-amt");
+
+    amtAfterReduc = (amtAfterReduc - savings) / 4;
+    sessionStorage.setItem("budget", amtAfterReduc);
+    console.log(amtAfterReduc);
 }
